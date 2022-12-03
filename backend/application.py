3684b08c295d00d5406b64069b06b51cb435d840
Giang Pham <giangpham@MacBook-Air-10.local>
2022-12-03 15:53:24 -0700

    changed the format of rules so they are consistent

--- backend/application.py
+++ backend/application.py
@@ -26,15 +26,16 @@ cas_client = CASClient(
     server_url='https://cas.coloradocollege.edu/cas/'
 )
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text + sso_link))
-
-
 # add a rule when the page is accessed with a name appended to the site
 # URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+@application.route('/<username>')
+def hello(username):
+    return header_text + say_hello(username) + home_link + footer_text
+
+# add a rule for the index page
+@application.route('/')
+def index():
+    return header_text + say_hello() + instructions + footer_text + sso_link
 
 @application.route('/profile')
 def profile(method=['GET']):
