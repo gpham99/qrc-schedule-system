613ae0b2c1091b72fc8d241af5bc4fb3e439cb47
Giang Pham <giangpham@MacBook-Air-10.local>
2022-12-03 15:48:17 -0700

    fixed redirect loop

--- backend/application.py
+++ backend/application.py
@@ -27,17 +27,14 @@ cas_client = CASClient(
 )
 
 # add a rule for the index page.
-# application.add_url_rule('/', 'index', (lambda: header_text +
-#     say_hello() + instructions + footer_text + sso_link))
+application.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + footer_text + sso_link))
 
-@application.route('/')
-def index():
-    return redirect(url_for('login'))
 
 # add a rule when the page is accessed with a name appended to the site
 # URL.
-# application.add_url_rule('/<username>', 'hello', (lambda username:
-#     header_text + say_hello(username) + home_link + footer_text))
+application.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
 
 @application.route('/profile')
 def profile(method=['GET']):
