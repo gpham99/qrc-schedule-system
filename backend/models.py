34b96bae55146f163cbc93d69b8bcc1e693f0ba4
Jessicat-H <jessica.hannebert@gmail.com>
2022-12-04 22:01:28 -0700

    Removed extra code from models.py

--- backend/models.py
+++ backend/models.py
@@ -11,12 +11,3 @@ class Tutor:
             "Lastname": self.lastname
         }
 
-# Generate marshmallow Schemas from your models
-class ArticlesShema(ma.Schema):
-    class Meta:
-        # Fields to expose
-        fields = ("id","title", "body", "date")
-
-
-article_schema = ArticlesShema()
-articles_schema = ArticlesShema(many=True)
