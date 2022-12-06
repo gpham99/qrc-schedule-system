bfdbedc5db955512b44ef28d6b6d6551bf1831d6
Pralad Mishra <p_mishra@coloradocollege.edu>
2022-12-05 20:58:12 -0700

    added a button to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import LoginButton from "./Components/LoginButton.js";
+import { useState, useEffect } from "react";
+import React from "react";
 
 function App() {
   return (
