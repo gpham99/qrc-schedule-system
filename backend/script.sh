a3105d062dcc8d2332f54fc3a49d67f25fa06b2b
Ubuntu <ubuntu@ip-172-31-8-85.us-west-2.compute.internal>
2022-12-07 18:19:44 +0000

    changed script.sh because we got rid of gunicorn

--- backend/script.sh
+++ backend/script.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-export FLASK_APP=/home/ubuntu/qss/backend/application.py
-flask run --host=0.0.0.0 --port=8080
+export FLASK_APP=/home/ubuntu/qrc-shedule-system/backend/application.py
+flask run --host=0.0.0.0 --port=5000
