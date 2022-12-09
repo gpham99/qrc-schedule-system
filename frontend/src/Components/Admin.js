9bea45e145caad69b085d5dd45857b4a25acc16c
Giang Pham <giangpham@MacBook-Air-34.local>
2022-12-08 22:09:46 -0700

    fixed the homepage to be balanced, created roster upload screen, make the color consistent

--- frontend/src/Components/Admin.js
+++ frontend/src/Components/Admin.js
@@ -9,7 +9,7 @@ import Internal from "./Admin/Internal";
 const Admin = () => {
   return (
     <div>
-      <div>
+      <div class="mt-5 p-2">
         <h1>Admin Routes</h1>
       </div>
 
@@ -41,13 +41,16 @@ const Admin = () => {
         </li>
       </ul>
 
-      <Routes>
-        <Route path="excel" element={<Roster></Roster>}></Route>
-        <Route path="tutor-info" element={<TutorInfo></TutorInfo>}></Route>
-        <Route path="schedule" element={<Schedule></Schedule>}></Route>
-        <Route path="time-window" element={<TimeWindow></TimeWindow>}></Route>
-        <Route path="internal" element={<Internal></Internal>}></Route>
-      </Routes>
+      <div class="p-5">
+        <Routes>
+          <Route path="" element={<Schedule></Schedule>}></Route>
+          <Route path="excel" element={<Roster></Roster>}></Route>
+          <Route path="tutor-info" element={<TutorInfo></TutorInfo>}></Route>
+          <Route path="schedule" element={<Schedule></Schedule>}></Route>
+          <Route path="time-window" element={<TimeWindow></TimeWindow>}></Route>
+          <Route path="internal" element={<Internal></Internal>}></Route>
+        </Routes>
+      </div>
     </div>
   );
 };
--- frontend/src/Components/Admin/Roster.js
+++ frontend/src/Components/Admin/Roster.js
@@ -2,7 +2,26 @@ import React from "react";
 
 const Roster = () => {
   return (
-    <div>Upload another Excel file or view the last one you uploaded.</div>
+    <div class="container align-items-center bg-light">
+      <div class="row p-4 justify-content-center">
+        <p>If your roster has any changes, please upload it here.</p>
+      </div>
+
+      <div class="row p-4 justify-content-center">
+        <a href="#">
+          <button type="button" class="btn btn-info">
+            View the last uploaded Excel sheet
+          </button>
+        </a>
+      </div>
+
+      <div class="row p-4 justify-content-center">
+        <form action="#">
+          <input type="file" id="myFile" name="filename" />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
   );
 };
 
