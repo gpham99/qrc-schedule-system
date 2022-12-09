9ce76c4145dcccc579670ccd8a58a6c6d9d49f4a
Giang Pham <giangpham@MacBook-Air-34.local>
2022-12-09 10:23:17 -0700

    fixed the frontend on the tutor's route

--- frontend/src/Components/Tutor.js
+++ frontend/src/Components/Tutor.js
@@ -6,27 +6,31 @@ import { Route, Routes } from "react-router-dom";
 const Tutor = () => {
   return (
     <div>
-      <div>
-        <h1>Admin Routes</h1>
+      <div class="mt-5 p-2">
+        <h1>Tutor Routes</h1>
       </div>
 
-      <ul class="nav justify-content-center">
-        <li class="nav-item">
-          <a class="nav-link active" href="/tutor/schedule">
-            Schedule
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/tutor/profile">
-            Profile
-          </a>
-        </li>
-      </ul>
+      <div>
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link active" href="/tutor/schedule">
+              Schedule
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/tutor/profile">
+              Profile
+            </a>
+          </li>
+        </ul>
+      </div>
 
-      <Routes>
-        <Route path="schedule" element={<Schedule></Schedule>}></Route>
-        <Route path="profile" element={<Profile></Profile>}></Route>
-      </Routes>
+      <div class="p-5">
+        <Routes>
+          <Route path="schedule" element={<Schedule></Schedule>}></Route>
+          <Route path="profile" element={<Profile></Profile>}></Route>
+        </Routes>
+      </div>
     </div>
   );
 };
