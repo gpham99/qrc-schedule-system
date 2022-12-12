5571c34b6b5515a596f6e98057ab638e82f1a4e9
Pralad Mishra <p_mishra@coloradocollege.edu>
2022-12-11 22:06:25 -0700

    schedule fetch added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,30 +5,34 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import Admin from "./Components/Admin";
 import Tutor from "./Components/Tutor";
+import Schedule from "./Components/Admin/Schedule";
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-  useEffect(() => {
-    fetch("http://52.12.35.11:8080/api/time")
-      .then((res) => res.json())
-      .then((data) => {
-        setCurrentTime(data.time);
-      });
-  }, []);
+  // const [currentSchedule, setCurrentSchedule] = useState([]);
+  // useEffect(() => {
+  //   fetch("http://52.12.35.11:8080/api/master_schedule")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setCurrentSchedule(data);
+  //     });
+  // }, []);
 
   return (
-    <div className="App">
-      {/* <div class="alert alert-info" role="alert">
-        The current time is {currentTime}.
-      </div> */}
-      <div>
-        <Routes>
-          <Route path="/admin/*" element={<Admin />}></Route>
-          <Route path="/tutor/*" element={<Tutor></Tutor>}></Route>
-          <Route path="/" element={<Home></Home>}></Route>
-        </Routes>
-      </div>
-    </div>
+    <Schedule></Schedule>
+    //     <div className="App">
+    //       <div class="d-flex flex-column">
+    //         The master schedule is {currentSchedule}.
+    //       </div>
+    //       <div>
+    //         {/* <MyComponent></MyComponent> */}
+    //         {/* <datePicker /> */}
+    //         {/* <Routes>
+    //           <Route path="/admin/*" element={<Admin />}></Route>
+    //           <Route path="/tutor/*" element={<Tutor></Tutor>}></Route>
+    //           <Route path="/" element={<Home></Home>}></Route>
+    //         </Routes> */}
+    //       </div>
+    //     </div>
   );
 }
 
--- frontend/src/Components/Admin/Schedule.js
+++ frontend/src/Components/Admin/Schedule.js
@@ -1,60 +1,99 @@
 import { Card, Container } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 const Schedule = () => {
+  const columns = [
+    {
+      Header: "Sunday",
+      accessor: "sunday",
+    },
+    {
+      Header: "Monday",
+      accessor: "monday",
+    },
+    {
+      Header: "Tuesday",
+      accessor: "tuesday",
+    },
+    {
+      Header: "Wednesday",
+      accessor: "wednesday",
+    },
+    {
+      Header: "Thursday",
+      accessor: "thursday",
+    },
+  ];
+  const [currentSchedule, setCurrentSchedule] = useState([]);
+  useEffect(() => {
+    fetch("http://52.12.35.11:8080/api/master_schedule")
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentSchedule(data);
+      });
+  }, []);
+
+  console.log(currentSchedule);
+
   return (
-    <div class="container p-5">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="table-responsive row justify-content center">
-            <table class="table table-bordered">
-              <thead class="table-dark">
-                <tr>
-                  <th></th>
-                  <th>Sunday</th>
-                  <th>Monday</th>
-                  <th>Tuesday</th>
-                  <th>Wednesday</th>
-                  <th>Thursday</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>2-4 PM</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>4-6 PM</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>6-8 PM</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>8-10 PM</td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+    // <div class="container p-5">
+    //   <div class="row">
+    //     <div class="col-md-12">
+    //       <div class="table-responsive row justify-content center">
+    //         <table class="table table-bordered">
+    //           <thead class="table-dark">
+    //             <tr>
+    //               <th></th>
+    //               <th>Sunday</th>
+    //               <th>Monday</th>
+    //               <th>Tuesday</th>
+    //               <th>Wednesday</th>
+    //               <th>Thursday</th>
+    //             </tr>
+    //           </thead>
+    //           <tbody>
+    //             <tr>
+    //               <td>2-4 PM</td>
+    //               <td>{currentSchedule[0]}</td>
+    //               <td>{currentSchedule[1]}</td>
+    //               <td>{currentSchedule[2]}</td>
+    //               <td>{currentSchedule[3]}</td>
+    //               <td>{currentSchedule[4]}</td>
+    //             </tr>
+    //             <tr>
+    //               <td>4-6 PM</td>
+    //               <td></td>
+    //               <td></td>
+    //               <td></td>
+    //               <td></td>
+    //               <td></td>
+    //             </tr>
+    //             <tr>
+    //               <td>6-8 PM</td>
+    //               <td></td>
+    //               <td></td>
+    //               <td></td>
+    //               <td></td>
+    //               <td></td>
+    //             </tr>
+    //             <tr>
+    //               <td>8-10 PM</td>
+    //               <td></td>
+    //               <td></td>
+    //               <td></td>
+    //               <td></td>
+    //               <td></td>
+    //             </tr>
+    //           </tbody>
+    //         </table>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div>
+      {Object.keys(currentSchedule).map((cell) => (
+        <p>{currentSchedule[cell]}</p>
+      ))}
     </div>
   );
 };
