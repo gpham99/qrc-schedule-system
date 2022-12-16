9d2e5d566421a0951c3dae62a096d66a8633b3f5
Ubuntu <ubuntu@ip-172-31-8-85.us-west-2.compute.internal>
2022-12-16 03:16:48 +0000

    Sanitize and display

--- backend/utility.py
+++ backend/utility.py
@@ -1,8 +1,14 @@
 #List of replacements for illegal SQL characters and what they are replacing
 SUBSTITUTIONS = [('_', ' '), ('$', '/')]
 
-#Replace convention characters with 
-def replace_chars(in_string):
+#Replace convention characters with characters for human display
+def display(in_string):
     for to_replace, replacement in SUBSTITUTIONS:
+        in_string = in_string.replace(to_replace, replacement)
+    return in_string
+
+#Replace illegal characters with convention characters
+def sanitize(in_string):
+    for replacement, to_replace in SUBSTITUTIONS:
         in_string = in_string.replace(to_replace, replacement)
     return in_string
