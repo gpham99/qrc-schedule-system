1299c756a39f8f5424bd184ca88b6f990f9767ea
Ubuntu <ubuntu@ip-172-31-8-85.us-west-2.compute.internal>
2022-12-17 21:38:33 +0000

    debug message

--- backend/application.py
+++ backend/application.py
@@ -410,6 +410,7 @@ def last_excel_file():
 
 @application.route('/api/set_time_window', methods=['POST'])
 def set_time_window():
+    print("Received call to set_time_window")
     time_data = request.get_json()
     start_time = time_data['start_time']
     end_time = time_data['end_time']
