74a7ebafd19badeab599692c0fe1635eab341a5a
Ubuntu <ubuntu@ip-172-31-8-85.us-west-2.compute.internal>
2022-12-19 20:03:57 +0000

    API fixes

--- backend/application.py
+++ backend/application.py
@@ -247,16 +247,22 @@ def get_tutor_schedule():
 @application.route('/api/tutor/update_info', methods = ['POST'])
 @jwt_required()
 def update_tutor_info():
+    ret = {}
     req = request.get_json()
-    new_shift_capacity = req['shift_capacity']
+    new_shift_capacity = int(req['shift_capacity'])
     new_disciplines = req['disciplines']
-    update_shift_capacity(current_identity.id, new_shift_capacity)
+    if new_shift_capacity >= 0:
+        update_shift_capacity(current_identity.id, new_shift_capacity)
+        ret['msg'] = 'Tutor info updated'
+    else:
+        update_shift_capacity(current_identity.id, 0)
+        ret['msg'] = 'Invalid shift capacity'
     disciplines = []
-    for discipline, discipline_bool in new_disciplines:
+    for discipline, discipline_bool in new_disciplines.values():
         if discipline_bool == True:
             disciplines.append(sanitize(discipline))
     update_tutoring_disciplines(current_identity.id, disciplines)
-    return "Tutor info updated"
+    return ret
 
 
 @application.route('/api/tutor/get_info', methods = ['GET'])
