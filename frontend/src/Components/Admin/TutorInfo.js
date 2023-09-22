039fb36b0a086bae92c299953bb74be30c33d535
Quynh Huong's Macbook <quynhhuongsmacbook@Quynhs-MacBook-Air.local>
2023-09-22 00:47:40 -0600

    Fixed the alert not closing in Tutor Info; can clear All now

--- frontend/src/Components/Admin/TutorInfo.js
+++ frontend/src/Components/Admin/TutorInfo.js
@@ -1,14 +1,26 @@
 import React, { useEffect, useState } from "react";
 
 const TutorInfo = () => {
+  const [tutors, setTutors] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+  const [submitError, setSubmitError] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const loadingErrorMessage = `We've encountered an issue while loading the tutors' statuses in this block.
+  Please try again, and if the problem persists, feel free to contact us for further assistance.
+  If you need to view the previous status of the table, you can simply reload the page.`
+  const submittingErrorMessage = `
+  Failed to save the changes in the table.
+  Please try again or contact support for assistance.
+  If you need to view the previous status of the table, you can simply reload the page.
+  `
 
-  const [tutorsInfo, setTutorsInfo] = useState({});
-
-  const [editMode, setEditMode] = useState(0); // 0 is false, 1 is true
-
-  const [edittedTutorsInfo, setEdittedTutorsInfo] = useState({});
-
-  const [clearAll, setClearAll] = useState(false);
+  const handleChange = (key, typeOfChange) => {
+    const tutorsCopy = {...tutors};
+    tutorsCopy[key][typeOfChange] = !tutorsCopy[key][typeOfChange];
+    setTutors(tutorsCopy);
+  }
 
   useEffect(() => {
       const requestOptions = {
@@ -16,199 +28,176 @@ const TutorInfo = () => {
           "Content-Type": "application/json",
         },
       };
-
-      fetch(
-        "http://44.230.115.148/api/get_tutors_information",
-        requestOptions
-      )
-        .then((response) => {
-          let res = response.json();
-          console.log("res: ", res);
-          return res;
-        })
-        .then((data) => {
-          if ("error" in data) {
-            console.log("An error occurred while trying to fetch profile information");
-          } else {
-            console.log("data: ", data);
-            setTutorsInfo(data);
-            setEdittedTutorsInfo({ ...data });
-          }
-        });
+      async function fetchTutors() {
+        try {
+          setLoading(true);
+          const res = await fetch("http://44.228.177.192/api/get_tutors_information", requestOptions);
+          const data = await res.json();
+          setTutors(data);
+        }
+        catch (e) {
+          setError(loadingErrorMessage);
+        }
+        finally {
+          setLoading(false);
+        }
+      }
+      fetchTutors();
     }
   , []);
 
-  const toggleEditMode = () => {
-    setEditMode(1 - editMode);
-    setClearAll(false);
-  };
-
-  const submitChange = () => {
-    if (clearAll) {
-      let edittedTutorsInfoCopy = {
-        ...edittedTutorsInfo,
-      };
-
-      for (const key in edittedTutorsInfoCopy) {
-        edittedTutorsInfoCopy[key]["this_block_unavailable"] = false;
-        edittedTutorsInfoCopy[key]["this_block_la"] = false;
-        edittedTutorsInfoCopy[key]["absence"] = false;
-      }
-
-      setEdittedTutorsInfo(edittedTutorsInfoCopy);
-    }
-
-    console.log("this is the editted tutors info: ", edittedTutorsInfo);
-
+  const submitChange = async () => {
     const requestOptions = {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(edittedTutorsInfo),
+      body: JSON.stringify(tutors),
     };
 
-    fetch(
-      "http://44.230.115.148/api/set_tutors_information",
-      requestOptions
-    )
-      .then((response) => {
-        let res = response.json();
-        return res;
-      })
-      .then((data) => {
-        console.log("finish submit data: ", data);
-      });
-
-    setEditMode(1 - editMode);
+    try {
+      setSubmitting(true);
+      const res = await fetch("http://44.228.177.192/api/set_tutors_information", requestOptions);
+      if (res.status === 200) {
+        setSuccess("Changes were saved to the database successfully!");
+      }
+      else {
+        setSubmitError(submittingErrorMessage);
+      }
+    }
+    catch (e) {
+      setSubmitError(submittingErrorMessage);
+    }
+    finally {
+      setSubmitting(false);
+    }
   };
 
+  const clearAll = () => {
+    const tutorsCopy = { ...tutors };
+    for (const tutorKey in tutorsCopy) {
+      if (tutorsCopy.hasOwnProperty(tutorKey)) {
+        tutorsCopy[tutorKey].absence = false;
+        tutorsCopy[tutorKey].this_block_la = false;
+        tutorsCopy[tutorKey].this_block_unavailable = false;
+      }
+    }
+    setTutors(tutorsCopy);
+  }
+
+  const clearLAStatus = () => {
+    const tutorsCopy = { ...tutors };
+    for (const tutorKey in tutorsCopy) {
+      if (tutorsCopy.hasOwnProperty(tutorKey)) {
+        tutorsCopy[tutorKey].this_block_la = false;
+      }
+    }
+    setTutors(tutorsCopy);
+  }
+
+  const clearUnavailable = () => {
+    const tutorsCopy = { ...tutors };
+    for (const tutorKey in tutorsCopy) {
+      if (tutorsCopy.hasOwnProperty(tutorKey)) {
+        tutorsCopy[tutorKey].this_block_unavailable = false;
+      }
+    }
+    setTutors(tutorsCopy);
+  }
+
+  const clearAbsence = () => {
+    const tutorsCopy = { ...tutors };
+    for (const tutorKey in tutorsCopy) {
+      if (tutorsCopy.hasOwnProperty(tutorKey)) {
+        tutorsCopy[tutorKey].absence = false;
+      }
+    }
+    setTutors(tutorsCopy);
+  }
+  
+  if (error) return <div className="container bg-light p-4">There was an error loading the table. Please reload to try again or contact for support.</div>
+
+  if (loading) return <div className="d-flex flex-column justify-content-center align-items-center bg-light p-4">
+    <div className="spinner-border text-info mb-4" role="status"></div>
+    <span>Loading Tutor Status table, please wait...</span>
+    </div>
+
   return (
-    <div class="container bg-light p-4">
-      {/* title and description */}
-      <div class="d-flex justify-content-center p-4">
-        {editMode === 0 ? (
-          <section>
-            <p class="text-left">
-              You can view the tutor's current Availablility status and LA
-              status.
-            </p>
-            <p class="text-left">To make any changes, go to Edit.</p>
-          </section>
-        ) : (
-          <section>
-            <p class="text-left">You are in Edit Mode right now.</p>
-          </section>
-        )}
+    <div className="container bg-light p-4">
+      <div className="d-flex justify-content-between p-4">
+          <button type="button" className="btn btn-info" onClick={() => {
+            submitChange();
+            setSubmitError("");
+            setError("");
+            setSuccess("");
+          }} disabled={submitting}>
+            {submitting ? "Saving..." : "Save Changes"}
+          </button>
+          <button type="button" className="btn btn-warning" disabled={submitting} onClick={clearAll}>Clear All</button>
       </div>
 
-      {/* pencil button */}
-      <div class="d-flex justify-content-between p-4">
-          {/* submit change button */}
-      {editMode === 1 && (
-        <>
-          <button type="button" className="btn btn-info" onClick={submitChange}>
-            Save Changes
-          </button>
-          <button type="button" className="btn btn-warning" onClick={() => setClearAll(true)}>Clear All</button>
-        </>
-      )}
-        {editMode === 0 ? (
-          <button class="btn btn-info" onClick={toggleEditMode}>
-            <span class="p-1"> Edit </span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-pencil-square"
-              viewBox="0 0 16 16"
-            >
-              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-              <path
-                fill-rule="evenodd"
-                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
-              />
-            </svg>
-          </button>
-        ) : (
-          <button class="btn btn-info" onClick={toggleEditMode}>
-            <span class="p-1"> Cancel </span>
-          </button>
-        )}
+      <div className="d-flex flex-row justify-content-end">
+          <button type="button" className="btn btn-link" onClick={clearLAStatus}>Clear LA Status</button>
+          <button type="button" className="btn btn-link" onClick={clearUnavailable}>Clear Unavailable</button>
+          <button type="button" className="btn btn-link" onClick={clearAbsence}>Clear Unexcused Absence</button>
       </div>
 
-      {/* table */}
-      <div class="p-4 table-responsive">
-        <table class="table table-bordered table-fixed">
-          <thead class="table-dark">
+      {submitError && 
+      <div className="alert alert-danger alert-dismissible" role="alert">
+        {submitError}
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>}
+
+      {success && 
+      <div className="alert alert-success alert-dismissible" role="alert">
+        {success}
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+      </div>}
+
+      <div className="p-4 table-responsive">
+        <table className="table table-bordered table-fixed">
+          <thead className="table-dark">
             <tr>
-              <th class="col-sm-4">Tutor</th>
-              <th class="col-sm-4">LA Status</th>
-              <th class="col-sm-4">Unavailable</th>
-              <th class="col-sm-4">Unex Ab</th>
+              <th className="col-sm-3">Tutor</th>
+              <th className="col-sm-3">LA Status</th>
+              <th className="col-sm-3">Unavailable</th>
+              <th className="col-sm-3">Unexcused Absence</th>
             </tr>
           </thead>
           <tbody>
-            {Object.keys(tutorsInfo).map((key) => (
-              <tr>
-                <td>{tutorsInfo[key]["name"]}</td>
+            {Object.keys(tutors).map((key) => (
+              <tr key={key}>
+                <td>{tutors[key]["name"]}</td>
                 <td>
                   {
                     <input
-                      class="form-check-input"
+                      className="form-check-input"
                       type="checkbox"
-                      value=""
-                      checked={clearAll ? false : edittedTutorsInfo[key]["this_block_la"]}
-                      disabled={editMode === 0 ? true : false}
-                      onChange={(e) => {
-                        let edittedTutorsInfoCopy = {
-                          ...edittedTutorsInfo,
-                        };
-                        edittedTutorsInfoCopy[key]["this_block_la"] =
-                          !edittedTutorsInfoCopy[key]["this_block_la"];
-                        setEdittedTutorsInfo(edittedTutorsInfoCopy);
-                      }}
+                      checked={tutors[key]["this_block_la"]}
+                      disabled={submitting}
+                      onChange={() => handleChange(key, "this_block_la")}
                     />
                   }
                 </td>
-
                 <td>
                   {
                     <input
-                      class="form-check-input"
+                      className="form-check-input"
                       type="checkbox"
-                      value=""
-                      checked={clearAll ? false : edittedTutorsInfo[key]["this_block_unavailable"]}
-                      disabled={editMode === 0 ? true : false}
-                      onChange={(e) => {
-                        let edittedTutorsInfoCopy = {
-                          ...edittedTutorsInfo,
-                        };
-                        edittedTutorsInfoCopy[key]["this_block_unavailable"] =
-                          !edittedTutorsInfoCopy[key]["this_block_unavailable"];
-                        setEdittedTutorsInfo(edittedTutorsInfoCopy);
-                      }}
+                      checked={tutors[key]["this_block_unavailable"]}
+                      disabled={submitting}
+                      onChange={() => handleChange(key, "this_block_unavailable")}
                     />
                   }
                 </td>
-
                 <td>
                   {
                     <input
-                      class="form-check-input"
+                      className="form-check-input"
                       type="checkbox"
-                      value=""
-                      checked={clearAll ? false : edittedTutorsInfo[key]["absence"]}
-                      disabled={editMode === 0 ? true : false}
-                      onChange={(e) => {
-                        let edittedTutorsInfoCopy = {
-                          ...edittedTutorsInfo,
-                        };
-                        edittedTutorsInfoCopy[key]["absence"] =
-                          !edittedTutorsInfoCopy[key]["absence"];
-                        setEdittedTutorsInfo(edittedTutorsInfoCopy);
-                      }}
+                      checked={tutors[key]["absence"]}
+                      disabled={submitting}
+                      onChange={() => handleChange(key, "absence")}
                     />
                   }
                 </td>
